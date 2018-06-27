39bd4149df797709aa0f5c9105b084c3f194c6bf
manojvarma <manojvarma132@gmail.com>
2018-06-27 09:22:33 +0530

    merged with server

--- src/config.js
+++ src/config.js
@@ -1,16 +1,15 @@
 module.exports = {
   server: {
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     port: 5522,
-    token: 'Ifyourserverisbehindareverseproxyyoumustwhitelisttheproxy'
+    token: "Ifyourserverisbehindareverseproxyyoumustwhitelisttheproxy"
   },
   database: {
-    host: 'localhost',
-    // host: "172.16.16.101",
-    db: 'rei',
-    username: 'root',
-    password: ''
+    host: "172.16.16.101",
+    db: "rei",
+    username: "root",
+    password: "EnMontoR"
   },
-  upload_folder: 'F:/REI/REI_SERVER/images/',
-  upload_Documents: 'G:/clgProject/REI_SERVER/documents/'
+  upload_folder: "F:/REI/REI_SERVER/images/",
+  upload_Documents: "G:/clgProject/REI_SERVER/documents/"
 };
