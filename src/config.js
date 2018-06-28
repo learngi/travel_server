0872e29fe4a2be2f18793cec252b1f6f93a997f7
manojvarma <manojvarma132@gmail.com>
2018-06-28 10:06:05 +0530

    changes in config

--- src/config.js
+++ src/config.js
@@ -2,13 +2,14 @@ module.exports = {
   server: {
     host: "127.0.0.1",
     port: 5522,
-    token: "Ifyourserverisbehindareverseproxyyoumustwhitelisttheproxy"
+    token: "vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy"
   },
   database: {
+    // host: "localhost",
     host: "172.16.16.101",
     db: "rei",
     username: "root",
-    password: "EnMontoR"
+    password: ""
   },
   upload_folder: "F:/REI/REI_SERVER/images/",
   upload_Documents: "G:/clgProject/REI_SERVER/documents/"
